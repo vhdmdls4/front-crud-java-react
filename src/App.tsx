cbbc94d50796c8b028b8e7633846bae31d55025f
Victor <vhduartelima@hotmail.com>
2024-05-12 19:15:38 -0300

    start: using vite with react-ts template

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import RouterProviderApp from "./routes";
+
+export default function App() {
+  //   <Provider store={store}>
+  //   <ErrorBoundary>
+  //     <PersistGate persistor={persistor}>
+  //       <AuthProvider>
+  //         <RouterProvider />
+  //       </AuthProvider>
+  //     </PersistGate>
+  //   </ErrorBoundary>
+  // </Provider>
+  <RouterProviderApp />;
+}
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routes/Root.tsx";
+import ErrorPage from "./errors/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
