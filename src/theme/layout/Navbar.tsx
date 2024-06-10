c3b57734153ce4a9a8837cf9eaf9d3b763757c66
Victor <vhduartelima@hotmail.com>
2024-06-10 08:09:25 -0300

    feat: added navbar

---
+++ src/theme/layout/Navbar.tsx
@@ -0,0 +1,74 @@
+import {
+  AppBar,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Theme,
+  Toolbar,
+  Typography,
+  makeStyles,
+} from "@mui/material";
+import { useState } from "react";
+import theme from "../Theme";
+
+function useStyles(theme: Theme) {
+  return makeStyles((theme: Theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    drawer: {
+      width: 240,
+    },
+  }));
+}
+
+export default function Navbar() {
+  const classes = useStyles(theme);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+          ></IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Your App Name
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer}>
+        <div
+          className={classes.drawer}
+          role="presentation"
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+        >
+          <List>
+            <ListItem>
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="About" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Contact" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
