58ca042188d30ebb24d52effcd5039c0d7e0519c
vhduartelima <vhduartelima@hotmail.com>
2024-06-11 16:59:30 -0300

    feat: added jest and ts dependecies to works, besides added testing library too

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectiories: ['node_modules', 'src'],
+};
