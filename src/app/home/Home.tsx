5d1197836bdcf5dba652cef96a11e67287648d56
Victor <vhduartelima@hotmail.com>
2024-06-22 12:46:20 -0300

    feat: features to test jest and data-testid

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [price, setPrice] = useState<number>(0);
+
+  const discount = 0.1;
+
+  function handleInputOnBlur(event: React.ChangeEvent<HTMLInputElement>) {
+    setPrice(Number(event.target.value));
+  }
+
+  function applyDiscount(price: number, discount: number) {
+    const discountedPrice = price - price * discount;
+    setPrice(discountedPrice);
+  }
+
   return (
-    <div>
+    <div className="flex flex-col">
       Home principal da aplicação
       <>Teste novamente</>
       <span>Testo</span>
+      <span>{price}</span>
+      <input type="number" onBlur={handleInputOnBlur} min={0} max={500} />
+      <button
+        id="applyDiscountButtonHomePage"
+        data-testid="applyDiscountButtonHomePage"
+        onClick={() => applyDiscount(price, discount)}
+      >
+        Click to apply discount
+      </button>
     </div>
   );
 }
