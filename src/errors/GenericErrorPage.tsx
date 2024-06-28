65aba643f06904bf1ac7bf5a513673c89c906a72
Victor <vhduartelima@hotmail.com>
2024-06-28 00:05:09 -0300

    update: improved generic error page

--- src/errors/GenericErrorPage.tsx
+++ src/errors/GenericErrorPage.tsx
@@ -1,15 +1,18 @@
 import { useRouteError } from "react-router-dom";
 
-export default function GenericErrorPage() {
+export default function ErrorBoundary() {
   const error: any = useRouteError();
   console.error(error);
+
+  // If you want to customize the component display name in React dev tools:
+  // ErrorBoundary.displayName = "SampleErrorBoundary";
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.status + " " + error.statusText || error.message || error}</i>
         <button onClick={() => window.history.back()}>Go Back</button>
       </p>
     </div>
